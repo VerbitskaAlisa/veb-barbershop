a875007ef8147071c01800b36f4f2b49a4085aed
VerbitskaAlisa <v.alica@ukr.net>
2022-12-16 11:23:23 +0200

    slider-done

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,34 @@
+const images = [];
+
+images[0] =  "url(../images/background-img/slider-img-1.jpg)";
+images[1] = "url(../images/background-img/slider-img-2.jpg)";
+images[2] = "url(../images/background-img/slider-img-3.jpg)";
+
+
+
 const refs = {
     changeBgcBtnBack: document.querySelector(".js-back"),
     changeBgcBtnForward: document.querySelector(".js-forward"),
     heroSection: document.querySelector(".hero-content"),
 }
 
-refs.changeBgcBtnBack.addEventListener("click", onChangeBgcBtn);
+refs.changeBgcBtnForward.addEventListener("click", onChangeBgcBtnForward);
+refs.changeBgcBtnBack.addEventListener('click', onChangeBgcBtnBack);
 
-function onChangeBgcBtn (e) {
-    console.log(e.currentTarget)
-   refs.heroSection.style.backgroundImage = "url(../images/background-img/new.jpg)"
+let i = 0;
+
+function onChangeBgcBtnForward () {
+       if( i >= images.length-1 ) i = -1;
+        i++;
+        return setBgImg();
+}
+
+function onChangeBgcBtnBack () {
+    if (i <= 0) i = images.length;
+      i--
+    return setBgImg();
+}
+
+function setBgImg() {
+    return refs.heroSection.style.backgroundImage = images[i];
 }
